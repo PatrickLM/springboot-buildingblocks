ae536e14c206283a39b30e98192e502309af9f6f
patricklm <patricklm@yahoo.com>
2020-05-19 18:12:17 +0200

    Second comment

--- src/main/java/com/learnrestservice/LearnrestserviceApplication.java
+++ src/main/java/com/learnrestservice/LearnrestserviceApplication.java
@@ -3,6 +3,7 @@ package com.learnrestservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class LearnrestserviceApplication {
 
